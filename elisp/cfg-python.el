02f9c42c17c67788a350c4bfa23b9e7ade299cca
rdbeni0 <12273445+rdbeni0@users.noreply.github.com>
2022-03-01 14:57:58 +0100

    DATE: 2022-03-01_14:57

--- elisp/cfg-python.el
+++ elisp/cfg-python.el
@@ -1,7 +1,7 @@
 ;;; cfg-python.el --- configfuration for python programming -*- lexical-binding: t -*-
 ;;; Commentary:
 
-;; Everything what is connected with python programming.
+;; Everything what is connected with python programming. But without LSP.
 
 ;;; Code:
 
@@ -10,6 +10,14 @@
   :defer t
   :config
   (add-hook 'python-mode-hook 'yapf-mode)
+  )
+
+(use-package anaconda-mode
+  :ensure t
+  :defer t
+  :config
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
   )
 
 (provide 'cfg-python)
