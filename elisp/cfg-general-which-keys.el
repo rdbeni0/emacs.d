099e7a2403d119f164b6019b477c36a0e67ca76b
rdbeni0 <12273445+rdbeni0@users.noreply.github.com>
2022-05-24 00:24:48 +0200

    DATE: 2022-05-24_00:24

--- elisp/cfg-general-which-keys.el
+++ elisp/cfg-general-which-keys.el
@@ -1071,6 +1071,19 @@
    "=s" '(php-set-style :which-key "set-style")
    )
 
+  ;; fish-mode
+
+  (general-define-key
+   :states '(normal visual emacs)
+   :keymaps 'fish-mode-map
+   :major-modes 'fish-mode
+   :prefix ","
+   ","  '(ffap :which-key "ffap")
+   "="  '(:ignore t :which-key "format")
+   "==" '(format-all-buffer :which-key "format-all-buffer")
+   "=t" '(fish_indent :which-key "fish_indent")
+   )
+
   ;;;;; for major modes ; the same hotkeys for all mentioned modes
 
   ;; comments
