74bd6bfb44967065209410b402148b4e7b10c662
rdbeni0 <12273445+rdbeni0@users.noreply.github.com>
2022-06-21 12:24:06 +0200

    DATE: 2022-06-21_12:24

--- elisp/cfg-fly-modes.el
+++ elisp/cfg-fly-modes.el
@@ -17,8 +17,10 @@
   :ensure t
   :init
   (global-flycheck-mode)
+
   ;; https://www.reddit.com/r/emacs/comments/gqymvz/how_to_force_flycheck_to_select_a_specific_syntax/
-  ;; (flycheck-add-next-checker 'python-flake8 'python-pylint)
+  ;; https://www.flycheck.org/en/latest/languages.html#python
+  (flycheck-add-next-checker 'python-flake8 'python-pylint)
   ;; (flycheck-add-next-checker 'python-flake8)
   )
 
