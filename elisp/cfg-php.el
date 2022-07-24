59007d24660897aea26eb61dec574e13b423f00c
rdbeni0 <12273445+rdbeni0@users.noreply.github.com>
2022-07-25 00:21:11 +0200

    DATE: 2022-07-25_00:21

--- elisp/cfg-php.el
+++ elisp/cfg-php.el
@@ -9,17 +9,17 @@
 
 (defun cfg/-php-format ()
   "Run custom PHP formatter on the current file (via CLI)."
-  (shell-command-on-region (point) (mark) (concat "prettier --parser php --print-width 180 --stdin-filepath " (buffer-file-name)) nil t)
+  (save-excursion
+    (shell-command-on-region (point) (mark) (concat "prettier --parser php --print-width 180 --stdin-filepath " (buffer-file-name)) nil t)
+    )
   )
 
 (defun cfg/php-custom-file-format ()
   "Format current php buffer."
   (interactive)
-  (save-excursion
-    (mark-whole-buffer)
-    (cfg/-php-format)
-    (message "Reformatted! In case of formatting errors, please undo.")
-    )
+  (mark-whole-buffer)
+  (cfg/-php-format)
+  (message "Reformatted! In case of formatting errors, please undo.")
   )
 
 (use-package php-mode
