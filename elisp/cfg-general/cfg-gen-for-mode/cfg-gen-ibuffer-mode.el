2f081428f65e2bb480108254492072ce9383edfb
rdbeni0 <12273445+rdbeni0@users.noreply.github.com>
2022-09-20 00:26:20 +0200

    DATE: 2022-09-20_00:26

--- elisp/cfg-general/cfg-gen-for-mode/cfg-gen-ibuffer-mode.el
+++ elisp/cfg-general/cfg-gen-for-mode/cfg-gen-ibuffer-mode.el
@@ -4,5 +4,5 @@
  :states '(normal visual emacs)
  :keymaps '(ibuffer-mode-map)
  :major-modes '(ibuffer-mode)
- "q"  '(kill-buffer-and-window :which-key "kill-buffer-and-window")
- "Q"  '(kill-this-buffer :which-key "kill-this-buffer"))
+ "q"  'kill-buffer-and-window
+ "Q"  'kill-this-buffer)
