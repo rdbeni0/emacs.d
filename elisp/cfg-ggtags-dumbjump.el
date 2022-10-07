e5d23a564869b8692c0627ea95dba3efb90af98e
rdbeni0 <12273445+rdbeni0@users.noreply.github.com>
2022-10-07 17:40:15 +0200

    DATE: 2022-10-07_17:40

--- elisp/cfg-ggtags-dumbjump.el
+++ elisp/cfg-ggtags-dumbjump.el
@@ -27,20 +27,22 @@
   :hook ((prog-mode . cfg/dumb-jump-activate))
   :init (defun cfg/dumb-jump-activate ()
           (interactive)
-	  ;; Note that the function xref-show-definitions-completing-read requires at least Xref 1.1.0.
-	  ;;  This can either be downloaded from ELPA or is bundled with Emacs 28.1 or newer.
-	  (if (version< emacs-version "28.1")
-	      ;; (message "Your emacs version is too old! dumb-jump will work only via \"dumb-jump-go\"")
-	    (load-file (expand-file-name "site-elisp/xref_dumb_jump.el" user-emacs-directory)))
-	  ;;
-	  ;; ^ if this msg will appear:
-	  ;; https://www.reddit.com/r/emacs/comments/pr7nh2/dumbjump/
-	  ;; "(If you really want to use the old interface, dumb-jump-use-legacy-interface can be set to nil as to silence the warnings you would get from using obsolete functions.
-	  ;; I would really recommend trying xref though.)"
-	  ;;
 	  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate nil t)
 	  (setq xref-show-definitions-function #'xref-show-definitions-completing-read))
   :config
+  ;; Note that the function xref-show-definitions-completing-read requires at least Xref 1.1.0.
+  ;; This can either be downloaded from ELPA or is bundled with Emacs 28.1 or newer.
+  (if (version< emacs-version "28.1")
+      ;; (message "Your emacs version is too old! dumb-jump-use-legacy-interface can be used!")
+      (load-file (expand-file-name "site-elisp/xref_dumb_jump.el" user-emacs-directory)))
+
+  ;;
+  ;; ^ alternative option:
+  ;; https://www.reddit.com/r/emacs/comments/pr7nh2/dumbjump/
+  ;; "(If you really want to use the old interface, dumb-jump-use-legacy-interface can be set to nil as to silence the warnings you would get from using obsolete functions.
+  ;; I would really recommend trying xref though.)"
+  ;;
+
   ;; https://github.com/jacktasia/dumb-jump#configuration
   )
 
