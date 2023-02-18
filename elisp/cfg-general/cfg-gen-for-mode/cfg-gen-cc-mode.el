d81c4277bbe6373130718bf9dfee27a319869628
rdbeni0 <rdbeni0.eu@gmail.com>
2023-02-18 01:31:28 +0100

    DATE: 2023-02-18_01:31

--- elisp/cfg-general/cfg-gen-for-mode/cfg-gen-cc-mode.el
+++ elisp/cfg-general/cfg-gen-for-mode/cfg-gen-cc-mode.el
@@ -6,6 +6,7 @@
  :major-modes '(cc-mode c-mode)
  :prefix ","
  "c"  '(compile :which-key "compile")
+ "a"  '(cfg/ffap :which-key "ffap")
  "="  '(:ignore t :which-key "format")
  "==" '(format-all-buffer :which-key "format-all-buffer")
  )
--- elisp/cfg-general/cfg-gen-for-mode/cfg-gen-fish-mode.el
+++ elisp/cfg-general/cfg-gen-for-mode/cfg-gen-fish-mode.el
@@ -5,7 +5,7 @@
  :keymaps 'fish-mode-map
  :major-modes 'fish-mode
  :prefix ","
- ","  '(ffap :which-key "ffap")
+ "a"  '(ffap :which-key "ffap")
  "="  '(:ignore t :which-key "format")
  "==" '(format-all-buffer :which-key "format-all-buffer")
  "=t" '(fish_indent :which-key "fish_indent"))
