a5d63255b6a75e194702a95cab26ba23a97f5e98
rdbeni0 <rdbeni0.eu@gmail.com>
2023-03-19 15:44:33 +0100

    DATE: 2023-03-19_15:44

--- elisp/cfg-yasnippets.el
+++ elisp/cfg-yasnippets.el
@@ -7,20 +7,33 @@
 
 ;;; Code:
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;; yasnippets
 ;; Documentation: https://joaotavora.github.io/yasnippet/
-
 (use-package yasnippet
   :ensure t
   :config
-  ;; add yasnippet to all backends:
+  ;; add yas-minor-mode per MAJOR mode - not global:
+  (add-hook 'php-mode-hook #'yas-minor-mode) ;; PHP
+  (add-hook 'emacs-lisp-mode-hook #'yas-minor-mode) ;; elisp
+  (add-hook 'text-mode-hook #'yas-minor-mode) ;; text files
+  (add-hook 'sh-mode-hook #'yas-minor-mode) ;; shell scripts, bash
+  (add-hook 'web-mode-hook #'yas-minor-mode) ;; front end
+  (add-hook 'c-mode-hook #'yas-minor-mode) ;; c files only
+  (add-hook 'cperl-mode-hook #'yas-minor-mode) ;; Perl
+  (add-hook 'python-mode-hook #'yas-minor-mode) ;; Python
+  (add-hook 'snippet-mode-hook #'yas-minor-mode) ;; snippets for yasnippets :-)
+  (add-hook 'org-mode-hook #'yas-minor-mode) ;; org-mode
+  (add-hook 'notmuch-message-mode-hook #'yas-minor-mode) ;; notmuch-message-mode
+  (yas-reload-all)
+  ;; alternative - add yasnippet to all backends:
   ;; (yas-global-mode 1)
   )
 
 ;; https://github.com/AndreaCrotti/yasnippet-snippets
+(use-package yasnippet-snippets
+  :ensure t
+)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Adding a custom/dynamic yasnippet directory:
 ;; https://stackoverflow.com/questions/46696009/adding-a-custom-yasnippet-directory-to-spacemacs
 ;; and dymamic dir - example of code:
@@ -36,24 +49,8 @@
 ;; and then:
 ;; (setq yas-snippet-dirs (append yas-snippet-dirs (list yasnippets-dynamic-data-dir)))
 ;;
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package yasnippet-snippets
-  :ensure t
-  :config
-  ;; add yas-minor-mode per MAJOR mode - not global:
-  (add-hook 'php-mode-hook #'yas-minor-mode) ;; PHP
-  (add-hook 'emacs-lisp-mode-hook #'yas-minor-mode) ;; elisp
-  (add-hook 'text-mode-hook #'yas-minor-mode) ;; text files
-  (add-hook 'sh-mode-hook #'yas-minor-mode) ;; shell scripts, bash
-  (add-hook 'web-mode-hook #'yas-minor-mode) ;; front end
-  (add-hook 'c-mode-hook #'yas-minor-mode) ;; c files only
-  (add-hook 'cperl-mode-hook #'yas-minor-mode) ;; Perl
-  (add-hook 'python-mode-hook #'yas-minor-mode) ;; Python
-  (add-hook 'snippet-mode-hook #'yas-minor-mode) ;; snippets for yasnippets :-)
-  (add-hook 'org-mode-hook #'yas-minor-mode) ;; org-mode
-  (add-hook 'notmuch-message-mode-hook #'yas-minor-mode) ;; notmuch-message-mode
-  (yas-reload-all))
 
 (provide 'cfg-yasnippets)
 ;;; cfg-yasnippets.el ends here
