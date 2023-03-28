060298f3f845c3109408c459eafa2bebf5216244
rdbeni0 <rdbeni0.eu@gmail.com>
2023-03-28 20:22:02 +0200

    DATE: 2023-03-28_20:22

--- elisp/cfg-general/cfg-gen-for-mode/cfg-gen-cc-mode.el
+++ elisp/cfg-general/cfg-gen-for-mode/cfg-gen-cc-mode.el
@@ -1,9 +1,9 @@
-;; general-cc-mode:
+;; general-cc-mode (c++, c):
 
 (general-define-key
  :states '(normal visual emacs)
- :keymaps '(cc-mode-map c-mode-map)
- :major-modes '(cc-mode c-mode)
+ :keymaps '(cc-mode-map c-mode-map c++-mode-map)
+ :major-modes '(cc-mode c-mode c++-mode)
  :prefix ","
  "c"  '(compile :which-key "compile")
  "="  '(:ignore t :which-key "format")
