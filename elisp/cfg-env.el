91e8cb4e1d2381ab7ef72a0d8d1c71dbb0e7e0b5
rdbeni0 <rdbeni0.eu@gmail.com>
2024-09-09 09:50:05 +0200

    DATE: 2024-09-09_09:50

--- elisp/cfg-env.el
+++ elisp/cfg-env.el
@@ -1,1 +1,1 @@
-/nix/store/zz37dvy05l72srakbr50a0gg203y1may-home-manager-files/DEV/emacs/emacs.d_default/elisp/cfg-env.el
+/nix/store/7b13l99yjaddcmy77k8drlz1psdmppdq-home-manager-files/DEV/emacs/emacs.d_default/elisp/cfg-env.el
