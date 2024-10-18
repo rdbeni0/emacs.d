24d625e9f5350421e786c6ef5e39ac7cdaba3e0e
rdbeni0 <rdbeni0.eu@gmail.com>
2024-10-18 20:00:14 +0200

    DATE: 2024-10-18_20:00

--- init.el
+++ init.el
@@ -1,12 +1,6 @@
 (load (expand-file-name "elisp/init_core.el" user-emacs-directory))
 
-;; optional core packages:
-(require 'cfg-op-core-dark-theme) ;; OK
-(require 'cfg-op-core-mode-line) ;; OK
-
-;;(require 'cfg-op-core-completion-systems) ;; OK
-(require 'cfg-op-completion-systems)
-(require 'cfg-op-projectile)
-(require 'cfg-op-treemacs-speedbar)
-
-
+;; Optional core packages:
+(require 'cfg-op-core-dark-theme)
+(require 'cfg-op-core-mode-line)
+(require 'cfg-op-core-completion-systems)
