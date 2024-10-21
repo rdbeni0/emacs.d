5deea077a1a52eee787e927861148fb73d659b3e
rdbeni0 <rdbeni0.eu@gmail.com>
2024-10-21 14:35:13 +0200

    DATE: 2024-10-21_14:35

---
+++ elisp/optional/cfg-op-epm.el
@@ -0,0 +1,13 @@
+;;; cfg-op-epm.el --- configfuration for epm -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Configuration for https://github.com/xuchunyang/epm
+;;
+;;; Code:
+
+(use-package epm
+  :ensure t
+  )
+
+(provide 'cfg-op-epm)
+;;; cfg-op-epm.el ends here
