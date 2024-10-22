14ffe3b21c34b9cf87395962b2260233d2e725b1
rdbeni0 <rdbeni0.eu@gmail.com>
2024-10-22 12:42:34 +0200

    DATE: 2024-10-22_12:42

--- elisp/core/cfg-custom-file.el
+++ elisp/core/cfg-custom-file.el
@@ -1,12 +1,10 @@
-;;; cfg-custom-file.el --- configfuration for custom variables/faces -*- lexical-binding: t -*-
+;;; cfg-custom-file.el --- pre configfuration for custom.el file -*- lexical-binding: t -*-
 ;;; Commentary:
-;;
-;; Configuration for custom.el file
 ;;
 ;;; Code:
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file) (load custom-file))
+;; (when (file-exists-p custom-file) (load custom-file))
 
 (provide 'cfg-custom-file)
 ;;; cfg-custom-file.el ends here
