baf813610dd9ef4280dc097b5d38ef1a650e4a95
rdbeni0 <rdbeni0.eu@gmail.com>
2024-10-23 16:47:38 +0200

    DATE: 2024-10-23_16:47

--- elisp/init_rich.el
+++ elisp/init_rich.el
@@ -4,8 +4,14 @@
 (require 'cfg-op-completion-systems)
 (require 'cfg-op-projectile)
 (require 'cfg-op-dumbjump)
-(require 'cfg-op-json)
 (require 'cfg-op-epm)
-(require 'cfg-op-markdown)
-(require 'cfg-op-format)
 (require 'cfg-op-doom-themes-fonts)
+
+;; Optional - langs:
+(require 'cfg-op-format)
+(require 'cfg-op-markdown)
+(require 'cfg-op-json)
+(require 'cfg-op-groovy-jenkins)
+(require 'cfg-op-nix)
+(require 'cfg-op-php)
+(require 'cfg-op-python)
