5b1a6e31795b3f21cdc4f55e963c8c4d500b837c
rdbeni0 <rdbeni0.eu@gmail.com>
2024-10-26 13:09:28 +0200

    DATE: 2024-10-26_13:09

--- elisp/optional/cfg-op-markdown.el
+++ elisp/optional/cfg-op-markdown.el
@@ -10,12 +10,12 @@
   :config
   (setq markdown-fontify-code-blocks-natively t)
   (setq markdown-command "pandoc")
+
   ;; https://github.com/Somelauw/evil-markdown
   ;; this is local package (should be loaded from site-elisp):
   (add-hook 'markdown-mode-hook (lambda ()
 				  (require 'evil-markdown)
-				  (evil-markdown-mode)
-				  ))
+				  (evil-markdown-mode)))
   ;; load general.el and keybindings:
   (require 'cfg-gen-op-markdown-mode))
 
