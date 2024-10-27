c0d470222ef93bbb96be2abdd315dd1ce65bf884
rdbeni0 <rdbeni0.eu@gmail.com>
2024-10-27 23:49:12 +0100

    DATE: 2024-10-27_23:49

--- elisp/abbrevs-tempo/at-abbrev_defs.el
+++ elisp/abbrevs-tempo/at-abbrev_defs.el
@@ -5,6 +5,8 @@
 ;;
 ;;; Code:
 
+(require 'tempo)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; TOC - TABLE OF CONTENT
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -21,19 +23,19 @@
 ;; (setq org-mode-abbrev-table nil)
 (define-abbrev-table 'org-mode-abbrev-table
   '(
-    ("qscss" "#+BEGIN_SRC css\n\n#+END_SRC\n" nil 0)
-    ("qsel" "#+BEGIN_SRC emacs-lisp\n\n#+END_SRC\n" nil 0)
-    ("qsjava" "#+BEGIN_SRC java\n\n#+END_SRC\n" nil 0)
-    ("qsphp" "#+BEGIN_SRC php\n\n#+END_SRC\n" nil 0)
-    ("qspl" "#+BEGIN_SRC perl\n\n#+END_SRC\n" nil 0)
-    ("qspy" "#+BEGIN_SRC python\n\n#+END_SRC\n" nil 0)
-    ("qssh" "#+BEGIN_SRC shell\n\n#+END_SRC\n" nil 0)
-    ("qssql" "#+BEGIN_SRC sql\n\n#+END_SRC\n" nil 0)
-    ("qslua" "#+BEGIN_SRC lua\n\n#+END_SRC\n" nil 0)
-    ("qqex" "#+BEGIN_EXAMPLE\n\n\#+END_EXAMPLE\n" nil 0)
-    ("qqsr" "#+BEGIN_SRC\n\n#+END_SRC\n" nil 0)
-    ("qqprop" ":PROPERTIES:\n:CUSTOM_ID: XXX\n:END:\n" nil 0)
-    ("qqqu" "#+BEGIN_QUOTE\n\n#+END_QUOTE\n" nil 0)
+    ("qscss" "#+BEGIN_SRC css\n\n#+END_SRC\n" nil 0 :system t)
+    ("qsel" "#+BEGIN_SRC emacs-lisp\n\n#+END_SRC\n" nil 0 :system t)
+    ("qsjava" "#+BEGIN_SRC java\n\n#+END_SRC\n" nil 0 :system t)
+    ("qsphp" "#+BEGIN_SRC php\n\n#+END_SRC\n" nil 0 :system t)
+    ("qspl" "#+BEGIN_SRC perl\n\n#+END_SRC\n" nil 0 :system t)
+    ("qspy" "#+BEGIN_SRC python\n\n#+END_SRC\n" nil 0 :system t)
+    ("qssh" "#+BEGIN_SRC shell\n\n#+END_SRC\n" nil 0 :system t)
+    ("qssql" "#+BEGIN_SRC sql\n\n#+END_SRC\n" nil 0 :system t)
+    ("qslua" "#+BEGIN_SRC lua\n\n#+END_SRC\n" nil 0 :system t)
+    ("qqex" "#+BEGIN_EXAMPLE\n\n\#+END_EXAMPLE\n" nil 0 :system t)
+    ("qqsr" "#+BEGIN_SRC\n\n#+END_SRC\n" nil 0 :system t)
+    ("qqprop" ":PROPERTIES:\n:CUSTOM_ID: XXX\n:END:\n" nil 0 :system t)
+    ("qqqu" "#+BEGIN_QUOTE\n\n#+END_QUOTE\n" nil 0 :system t)
     ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -43,9 +45,9 @@
 ;; (setq emacs-lisp-mode-abbrev-table nil)
 (define-abbrev-table 'emacs-lisp-mode-abbrev-table
   '(
-    ("qqatt" "(define-abbrev-table 'XXX-mode-abbrev-table\n\  '(\n    (\"te\" \"test\" nil 0)\n    ))" nil 0) ;; define new at
-    ("qqata" "    (\"qq\" \"#\\n\" nil 0)" nil 0) ;; add element to the at
-    ("qqatd" "(define-abbrev XXX-mode-abbrev-table \"qq\" \"\\n\")" nil 0)
+    ("qqatt" "(define-abbrev-table 'XXX-mode-abbrev-table\n\  '(\n    (\"te\" \"test\" nil 0 :system t)\n    ))" nil 0 :system t) ;; define new at
+    ("qqata" "    (\"qq\" \"#\\n\" nil 0 :system t)" nil 0 :system t) ;; add element to the at
+    ("qqatd" "(define-abbrev XXX-mode-abbrev-table \"qq\" \"\\n\" :system t)" nil 0 :system t)
     ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -55,12 +57,12 @@
 ;; (setq cperl-mode-abbrev-table nil)
 (define-abbrev-table 'cperl-mode-abbrev-table
   '(
-    ("qqs" "#!/usr/bin/env bash\n# -*- coding: utf-8 -*-\n\nuse strict;\nuse warnings;\n" nil 0)
-    ("qqsubh" "sub help {\n    print \"Usage: ...\\n\";\n    exit;\n}" nil 0)
-    ("qqif" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n}" nil 0)
-    ("qqife" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n} else {\n    # code here\n}" nil 0)
-    ("qqife1" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n} elsif (CO) {\n    # code here\n} else {\n    # code here\n}" nil 0)
-    ("qqife2" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n} elsif (CO) {\n    # code here\n} elsif (CO) {\n    # code here\n} else {\n    # code here\n}" nil 0)
+    ("qqs" "#!/usr/bin/env perl\n# -*- coding: utf-8 -*-\n\nuse strict;\nuse warnings;\n" nil 0 :system t)
+    ("qqsubh" "sub help {\n    print \"Usage: ...\\n\";\n    exit;\n}" nil 0 :system t)
+    ("qqif" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n}" nil 0 :system t)
+    ("qqife" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n} else {\n    # code here\n}" nil 0 :system t)
+    ("qqife1" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n} elsif (CO) {\n    # code here\n} else {\n    # code here\n}" nil 0 :system t)
+    ("qqife2" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n} elsif (CO) {\n    # code here\n} elsif (CO) {\n    # code here\n} else {\n    # code here\n}" nil 0 :system t)
     ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -68,16 +70,34 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (setq python-mode-abbrev-table nil)
+
 (define-abbrev-table 'python-mode-abbrev-table
   '(
-    ("qqs" "#!/usr/bin/env python\n# -*- coding: utf-8 -*-" nil 0)
-    ("qqdefh" "def help():\n    print('X')\n    exit()\n" nil 0)
-    ("qqif" "#COnditions: == != > >= <= and or not\nif (CO):\n    X" nil 0)
-    ("qqife" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelse:\n    X" nil 0)
-    ("qqife1" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelif (CO):\n    X\nelse:\n    X" nil 0)
-    ("qqife2" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelif (CO):\n    X\nelif (CO):\n    X\nelse:\n    X" nil 0)
-    ("qqimp" "import re, os, sys\n" nil 0)
+    ("qqs" "#!/usr/bin/env python\n# -*- coding: utf-8 -*-" nil 0 :system t)
+    ("qqdefh" "def help():\n    print('X')\n    exit()\n" nil 0 :system t)
+    ("qqif" "#COnditions: == != > >= <= and or not\nif (CO):\n    X" nil 0 :system t)
+    ("qqife" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelse:\n    X" nil 0 :system t)
+    ("qqife1" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelif (CO):\n    X\nelse:\n    X" nil 0 :system t)
+    ("qqife2" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelif (CO):\n    X\nelif (CO):\n    X\nelse:\n    X" nil 0 :system t)
+    ("qqimp" "import re, os, sys\n" nil 0 :system t)
     ))
+
+(define-skeleton python-argparse-skeleton
+  "Insert a Python argparse example with def main()."
+  nil
+  "\n#!/usr/bin/env python\n"
+  "# -*- coding: utf-8 -*-\n\n"
+  "import argparse\n\n"
+  "def main():\n"
+  "    parser = argparse.ArgumentParser()\n"
+  "    parser.add_argument(\"-t\", \"--test\", required=True, help=\"Test param\")\n"
+  "    parser.add_argument(\"-a\", \"--age\", required=False, type=int, help=\"Foo\")\n\n"
+  "    args = parser.parse_args()\n\n"
+  "    print(f\"Hello, {args.test}! You are {args.age} years old.\")\n\n\n"
+  "if __name__ == \"__main__\":\n"
+  "    main()\n")
+
+(define-abbrev python-mode-abbrev-table "qqdefargp" "#SKELETON > Insert a Python argparse example with def main()." 'python-argparse-skeleton :system t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; SH-MODE
@@ -86,7 +106,7 @@
 ;; (setq sh-mode-abbrev-table nil)
 (define-abbrev-table 'sh-mode-abbrev-table
   '(
-    ("qqs" "#!/usr/bin/env bash\n# -*- coding: utf-8 -*-" nil 0)
+    ("qqs" "#!/usr/bin/env bash\n# -*- coding: utf-8 -*-" nil 0 :system t)
     ))
 
 (provide 'at-abbrev_defs)
