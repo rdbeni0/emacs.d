0b046a14d535ed6fec1361ee00ec8b0a1abdfdc7
rdbeni0 <rdbeni0.eu@gmail.com>
2024-10-28 23:37:52 +0100

    DATE: 2024-10-28_23:37

--- elisp/abbrevs-tempo/at-abbrev_defs.el
+++ elisp/abbrevs-tempo/at-abbrev_defs.el
@@ -17,7 +17,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Shortcuts / namespaces
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Here you will find general rules and shortcuts (namespaces) for abbrevs. 
+;; Here you will find general rules and shortcuts (namespaces) for abbrevs.
 ;; The shortcuts will be based on C or C++ (e.g. `include' as `qqinc' instead of import as `qqimp').
 ;;
 ;; qqdb   :  databases, sql
@@ -26,13 +26,14 @@
 ;; qqfun  :  function; definitions
 ;; qqif   :  if...else if... else statements (overall conditionals)
 ;; qqinc  :  include; loading external libraries
-;; qqst   :  start; everything related to the beginning of the file, e.g. shebang or declarations for editor settings
+;; qqsta  :  start; everything related to the beginning of the file, e.g. shebang or declarations for editor settings
 ;; qqstr  :  string; manipulations with strings
 ;; qqsys  :  sys, system(); run commands
+;; qqopt  :  options; getopt - parsing program options (CLI args and flags)
 ;; qqte   :  tests
 ;; qqurl  :  url; curl, http/rest requests
 ;;
-;; qqsk   : skeletons; larger code templates
+;; qsk    : skeletons; larger code templates
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; ORG-MODE
@@ -75,7 +76,7 @@
 ;; (setq cperl-mode-abbrev-table nil)
 (define-abbrev-table 'cperl-mode-abbrev-table
   '(
-    ("qqst" "#!/usr/bin/env perl\n# -*- coding: utf-8 -*-\n\nuse strict;\nuse warnings;\n" nil 0 :system t)
+    ("qqsta" "#!/usr/bin/env perl\n# -*- coding: utf-8 -*-\n\nuse strict;\nuse warnings;\n" nil 0 :system t)
     ("qqfunh" "sub help {\n    print \"Usage: ...\\n\";\n    exit;\n}" nil 0 :system t)
     ("qqif" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n}" nil 0 :system t)
     ("qqife" "# COnditions: == != > >= <= eq ne gt ge lt le\nif (CO) {\n    # code here\n} else {\n    # code here\n}" nil 0 :system t)
@@ -90,13 +91,15 @@
 ;; (setq python-mode-abbrev-table nil)
 (define-abbrev-table 'python-mode-abbrev-table
   '(
-    ("qqst" "#!/usr/bin/env python\n# -*- coding: utf-8 -*-" nil 0 :system t)
+    ("qqsta" "#!/usr/bin/env python\n# -*- coding: utf-8 -*-" nil 0 :system t)
     ("qqfunh" "def help():\n    print('X')\n    exit()\n" nil 0 :system t)
     ("qqif" "#COnditions: == != > >= <= and or not\nif (CO):\n    X" nil 0 :system t)
     ("qqife" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelse:\n    X" nil 0 :system t)
     ("qqife1" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelif (CO):\n    X\nelse:\n    X" nil 0 :system t)
     ("qqife2" "#COnditions: == != > >= <= and or not\nif (CO):\n    X\nelif (CO):\n    X\nelif (CO):\n    X\nelse:\n    X" nil 0 :system t)
     ("qqinc" "import re, os, sys\n" nil 0 :system t)
+    ("qqfiw1" "with open(\'X\', \'a\') as file:\n    file.write(\"\\nNew Line\")" nil 0 :system t)
+    ("qqfir1" "with open(\'X\', \'r\') as file:\n    for line in file:\n        print(line.strip())\n" nil 0 :system t)
     ))
 
 (define-skeleton python-argparse-skeleton
@@ -114,7 +117,19 @@
   "if __name__ == \"__main__\":\n"
   "    main()\n")
 
-(define-abbrev python-mode-abbrev-table "qqskargp" "#SKELETON > Insert a Python argparse example with def main()." 'python-argparse-skeleton :system t)
+(define-skeleton python-file-replace-skeleton
+  "Insert Python code to replace text line by line in a file."
+  nil
+  "\n#SKELETON> In the case of 'with open (...)' file.close() is automatic (not required).\n"
+  "with open('X', 'r') as file:\n"
+  "    lines = file.readlines()\n\n"
+  "with open('X', 'w') as file:\n"
+  "    for line in lines:\n"
+  "        new_line = line.replace(\"" (skeleton-read "Old text: ") "\", \"" (skeleton-read "New text: ") "\")\n"
+  "        file.write(new_line)\n")
+
+(define-abbrev python-mode-abbrev-table "qskopt1" "#SKELETON> Argparse - example with def main()." 'python-argparse-skeleton :system t)
+(define-abbrev python-mode-abbrev-table "qskfi1" "#SKELETON> File handling - replace text line by line in a file." 'python-file-replace-skeleton :system t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; SH-MODE
@@ -123,7 +138,7 @@
 ;; (setq sh-mode-abbrev-table nil)
 (define-abbrev-table 'sh-mode-abbrev-table
   '(
-    ("qqst" "#!/usr/bin/env bash\n# -*- coding: utf-8 -*-" nil 0 :system t)
+    ("qqsta" "#!/usr/bin/env bash\n# -*- coding: utf-8 -*-" nil 0 :system t)
     ))
 
 (provide 'at-abbrev_defs)
