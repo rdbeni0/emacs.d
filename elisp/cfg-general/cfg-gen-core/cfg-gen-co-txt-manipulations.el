17db59db97be2bd74d9f19455a9538241c3de81a
rdbeni0 <rdbeni0.eu@gmail.com>
2024-11-27 18:25:44 +0100

    DATE: 2024-11-27_18:25

--- elisp/cfg-general/cfg-gen-core/cfg-gen-co-txt-manipulations.el
+++ elisp/cfg-general/cfg-gen-core/cfg-gen-co-txt-manipulations.el
@@ -5,8 +5,8 @@
  :keymaps list-gen-mode-map-txtman
  :major-modes list-gen-mode-txtman
  :prefix ","
- "."  '(:ignore t :which-key "txt-man")
- ".%" '(string-rectangle :which-key "string-rectangle")
+ "."   '(:ignore t :which-key "txt-man")
+ ".%"  '(string-rectangle :which-key "string-rectangle")
  ".'"  '(transpose-words :which-key "swap-words")
  ".;"  '(sort-words :which-key "sort-words")
  ".A"  '(align-regexp :which-key "align-regexp")
@@ -21,14 +21,14 @@
  ".e"  '(cfg/duplicate-current-line-or-region :which-key "dup-line-or-reg")
  ".g"  '(fill-paragraph :which-key "fill-paragraph")
  ".j"  '(cfg/join-lines-in-region-add-spc :which-key "join-lines-spc")
- ".l" '(kill-whole-line :which-key "kill-line")
+ ".l"  '(kill-whole-line :which-key "kill-line")
  ".n"  '(transpose-lines :which-key "swap-lines")
  ".p"  '(delete-duplicate-lines :which-key "del-dup-lines")
  ".r"  '(reverse-region :which-key "reverse-lines")
  ".s"  '(sort-lines :which-key "sort-lines")
  ".t"  '(cfg/toggle-case :which-key "toggle-case")
  ".u"  '(cfg/dos2unix :which-key "dos2unix")
- ".w" '(delete-trailing-whitespace :which-key "delete-tr-whitespace")
+ ".w"  '(delete-trailing-whitespace :which-key "delete-tr-whitespace")
  ".y"  '(untabify :which-key "untabify")
  ".z"  '(cfg/capitalize-region :which-key "capitalize"))
 
