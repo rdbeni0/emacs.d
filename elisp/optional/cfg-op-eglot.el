03b397749ada4146b12ababa1e8ae1d30b7db03f
rdbeni0 <rdbeni0.eu@gmail.com>
2024-12-25 14:05:27 +0100

    DATE: 2024-12-25_14:05

---
+++ elisp/optional/cfg-op-eglot.el
@@ -0,0 +1,11 @@
+;;; cfg-op-eglot.el --- eglot -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Optional settings for eglot in emacs.
+;;
+;;; Code:
+
+;; if the eglot is slow: https://github.com/jdtsmith/eglot-booster
+
+(provide 'cfg-op-eglot)
+;;; cfg-op-eglot.el ends here
