91a9d359b135356f0713d7647c2b21b810765619
rdbeni0 <rdbeni0.eu@gmail.com>
2024-12-29 19:18:10 +0100

    DATE: 2024-12-29_19:18

--- elisp/cfg-general/cfg-gen-optional/cfg-gen-op-emmet.el
+++ elisp/cfg-general/cfg-gen-optional/cfg-gen-op-emmet.el
@@ -1,5 +1,13 @@
 ;;; cfg-gen-op-emmet.el --- general.el for emmet -*- lexical-binding: t -*-
 
+;; should be used with combination with web-mode:
+(general-define-key
+ :states '(normal visual emacs)
+ :keymaps '(emmet-mode-map)
+ :major-modes '(emmet-mode)
+ :prefix ","
+ "m"  '(emmet-expand-line :which-key "emmet-expand")
+ "M"  '(emmet-preview :which-key "emmet-preview"))
 
 
 (provide 'cfg-gen-op-emmet)
--- elisp/optional/cfg-op-emmet.el
+++ elisp/optional/cfg-op-emmet.el
@@ -7,16 +7,12 @@
   :ensure t
   :init
   :config
-  ;; Code to run after package is loaded
-  (my-package-initialize)
-  :bind
+  ;; load general.el and keybindings:
+  (require 'cfg-gen-op-emmet)
   :hook
   (web-mode-hook . emmet-mode)
-  my-package-command
-  :defer t
-  ;; Defer loading until needed
+  :defer t ;; Defer loading until needed
   :after (web-mode))
-
 
 (provide 'cfg-op-emmet)
 ;;; cfg-op-emmet.el ends here
