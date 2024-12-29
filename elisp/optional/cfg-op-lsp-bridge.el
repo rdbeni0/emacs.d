309d5617ef2280a0792ed43e818437c4ebdf9c7a
rdbeni0 <rdbeni0.eu@gmail.com>
2024-12-29 01:25:43 +0100

    DATE: 2024-12-29_01:25

--- elisp/optional/cfg-op-lsp-bridge.el
+++ elisp/optional/cfg-op-lsp-bridge.el
@@ -13,17 +13,58 @@
 ;;
 ;; https://www.reddit.com/r/emacs/comments/vhqewj/lspbridge_is_good/
 ;;
-;; acm - Asynchronous Completion Menu
+;; "acm" - Asynchronous Completion Menu
 ;;; Code:
 
 ;; https://github.com/manateelazycat/lsp-bridge?tab=readme-ov-file#lsp-server-options
 ;; https://github.com/manateelazycat/lsp-bridge?tab=readme-ov-file#options
 (use-package lsp-bridge
   :config
+  ;; use global-mode, but disable per particular mode:
   (global-lsp-bridge-mode)
+
+  ;; popup menu seems to be broken, better to use standard completion at the bottom:
+  (setq lsp-bridge-code-action-enable-popup-menu nil)
+
+  ;; multiserver - custom user config and json files:
+  (setq lsp-bridge-user-multiserver-dir (expand-file-name "data/lsp-bridge" user-emacs-directory))
+
+  ;; optional debug if something is wrong
+  ;; (setq lsp-bridge-enable-log t)
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;; Exclusion from selected major-modes:
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (setq lsp-bridge-default-mode-hooks
+        (remove 'cperl-mode-hook lsp-bridge-default-mode-hooks))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;; php
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (setq lsp-bridge-php-lsp-server "phpactor")
 
-  ;; load general.el and keybindings:
+  ;; (add-hook 'php-mode-hook
+  ;;           (lambda ()
+  ;; 	      (lsp-bridge-mode)))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;; python
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; `basedpyright' is a fork of pyright with various type checking improvements, improved vscode support and pylance features built into the language server.
+  ;; `basedpyright' seems to be better than `pyright', but also very similar
+  ;; And as failback - we can try `pylsp'
+  ;; pyright/basedpyright - currently "code actions" are broken, so must be executed with `ruff-lsp' (so as multiserver)
+
+  (setq lsp-bridge-python-multi-lsp-server "basedpyright_ruff") ;; so in summary - this is the best option for now
+
+  ;; (add-hook 'python-mode-hook
+  ;;           (lambda ()
+  ;; 	      (lsp-bridge-mode)))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;; Load keybindings:
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (require 'cfg-gen-op-lsp-bridge))
 
 ;; https://github.com/manateelazycat/lsp-bridge?tab=readme-ov-file#customize-language-server-configuration-file
