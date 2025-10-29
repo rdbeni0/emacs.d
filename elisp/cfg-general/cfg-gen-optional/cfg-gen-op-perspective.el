85f84c2faacbcb12942ee51ce0469ec035009404
rdbeni0 <rdbeni0.eu@gmail.com>
2025-10-29 20:07:12 +0100

    DATE: 2025-10-29_20:07

--- elisp/cfg-general/cfg-gen-optional/cfg-gen-op-perspective.el
+++ elisp/cfg-general/cfg-gen-optional/cfg-gen-op-perspective.el
@@ -1,6 +1,7 @@
 ;;; cfg-gen-op-perspective.el --- general.el for perspective -*- lexical-binding: t -*-
 
 ;; https://stackoverflow.com/questions/58615798/how-to-use-leader-key-as-part-of-package-prefix
+(define-key evil-normal-state-map (kbd "SPC b n") 'perspective-map)
 (define-key evil-normal-state-map (kbd "SPC b p") 'perspective-map)
 
 
