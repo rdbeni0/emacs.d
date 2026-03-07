c4257d6c6b0e00c613f2a4d088ef1389bdcf463d
rdbeni0 <rdbeni0.eu@gmail.com>
2026-03-07 14:35:25 +0100

    DATE: 2026-03-07_14:35

--- elisp/optional/cfg-op-python.el
+++ elisp/optional/cfg-op-python.el
@@ -22,55 +22,56 @@
 (use-package sphinx-doc
   :ensure t
   :config
-  (add-hook 'python-mode-hook
-	    (lambda ()
-              (require 'sphinx-doc)
-	      (sphinx-doc-mode t))))
-
-;;;; flycheck: https://www.flycheck.org/en/latest/languages.html#python
-;; we need to know that flycheck has excellent support for python-mode in emacs,
-;; but it needs some executables to be installed
-(when (require 'flycheck nil 'noerror)
-
-  ;; Additional `flycheck-*' variables:
-  ;;
-  ;; (setq flycheck-python-pylint-executable "~/.local/bin/pylint")
-  ;; (setq flycheck-python-mypy-executable "~/.local/bin/mypy")
-  ;; (setq flycheck-python-ruff-executable "/run/current-system/sw/bin/ruff")
-  ;; (setq flycheck-pylintrc (expand-file-name ".pylintrc" user-emacs-directory))
-
-  ;; https://github.com/python/mypy
-  (setq flycheck-python-mypy-cache-dir
-        (expand-file-name ".cache/mypy" user-emacs-directory))
-
-  ;; Ruff additional arguments: turn off line length
-  (setq flycheck-python-ruff-args '("--ignore=E501"))
-
   (dolist (py-hook '(python-mode-hook python-ts-mode-hook))
     (add-hook py-hook
-              (lambda ()
-                ;; We usually do NOT use ruff like we do in LSP.
-                ;; But sometimes implementation in LSP can be broken:
-                (flycheck-select-checker 'python-ruff)
-                ;;
-                ;; https://github.com/pylint-dev/pylint
-                ;; (flycheck-select-checker 'python-pylint)
-                ;;
-                )))
+	      (lambda ()
+                (require 'sphinx-doc)
+	        (sphinx-doc-mode t))))
 
-  ;; Setting the checker order
-  (with-eval-after-load 'flycheck
-    ;; Do NOT use ruff and pyright: this is good, but it duplicates with LSP:
-    ;; (flycheck-add-next-checker 'python-ruff 'python-pyright)
+;;;; flycheck: https://www.flycheck.org/en/latest/languages.html#python
+  ;; we need to know that flycheck has excellent support for python-mode in emacs,
+  ;; but it needs some executables to be installed
+  (when (require 'flycheck nil 'noerror)
+
+    ;; Additional `flycheck-*' variables:
     ;;
-    (flycheck-add-next-checker 'python-ruff 'python-pylint)
-    ;; So in this case we will use other engines and linters:
-    (flycheck-add-next-checker 'python-pylint 'python-flake8)
-    (flycheck-add-next-checker 'python-flake8 'python-pycompile 'python-mypy)
-    ))
+    ;; (setq flycheck-python-pylint-executable "~/.local/bin/pylint")
+    ;; (setq flycheck-python-mypy-executable "~/.local/bin/mypy")
+    ;; (setq flycheck-python-ruff-executable "/run/current-system/sw/bin/ruff")
+    ;; (setq flycheck-pylintrc (expand-file-name ".pylintrc" user-emacs-directory))
 
-;; load general.el and keybindings:
-(require 'cfg-gen-op-python-mode)
+    ;; https://github.com/python/mypy
+    (setq flycheck-python-mypy-cache-dir
+          (expand-file-name ".cache/mypy" user-emacs-directory))
 
-(provide 'cfg-op-python)
+    ;; Ruff additional arguments: turn off line length
+    (setq flycheck-python-ruff-args '("--ignore=E501"))
+
+    (dolist (py-hook '(python-mode-hook python-ts-mode-hook))
+      (add-hook py-hook
+                (lambda ()
+                  ;; We usually do NOT use ruff like we do in LSP.
+                  ;; But sometimes implementation in LSP can be broken:
+                  (flycheck-select-checker 'python-ruff)
+                  ;;
+                  ;; https://github.com/pylint-dev/pylint
+                  ;; (flycheck-select-checker 'python-pylint)
+                  ;;
+                  )))
+
+    ;; Setting the checker order
+    (with-eval-after-load 'flycheck
+      ;; Do NOT use ruff and pyright: this is good, but it duplicates with LSP:
+      ;; (flycheck-add-next-checker 'python-ruff 'python-pyright)
+      ;;
+      (flycheck-add-next-checker 'python-ruff 'python-pylint)
+      ;; So in this case we will use other engines and linters:
+      (flycheck-add-next-checker 'python-pylint 'python-flake8)
+      (flycheck-add-next-checker 'python-flake8 'python-pycompile 'python-mypy)
+      ))
+
+  ;; load general.el and keybindings:
+  (require 'cfg-gen-op-python-mode)
+
+  (provide 'cfg-op-python)
 ;;; cfg-op-python.el ends here
