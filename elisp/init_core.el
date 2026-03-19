0d2a902f11c5cd709c021e3cdc5ebfe4ee882989
rdbeni0 <rdbeni0.eu@gmail.com>
2026-03-19 19:05:50 +0100

    DATE: 2026-03-19_19:05

--- elisp/init_core.el
+++ elisp/init_core.el
@@ -752,18 +752,21 @@ Uses position instead of index field."
   (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; -> IBUFFER
+;;;; -> IBUFFER AND BUFFERS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; https://olddeuteronomy.github.io/post/emacs-ibuffer-config/
-;;
 
+;; https://olddeuteronomy.github.io/post/emacs-ibuffer-config/
 (use-package ibuffer
   :functions
-  (cfg/ibuffer-disable-mouse-tooltips)
+  (cfg/ibuffer-disable-mouse-tooltips
+   cfg/alternate-buffer
+   cfg/revert-buffer
+   cfg/switch-to-buffer)
   :config
   ;; turn off prompts "yes" or "no":
   (setq ibuffer-expert t)
+
+  ;; defuns
 
   (defun cfg/ibuffer-disable-mouse-tooltips ()
     "Disable mouse tooltips (help-echo) in `ibuffer-mode`."
@@ -774,6 +777,18 @@ Uses position instead of index field."
 
   (add-hook 'ibuffer-mode-hook #'cfg/ibuffer-disable-mouse-tooltips)
 
+  (defun cfg/alternate-buffer (&optional window)
+    "Switch back and forth between the current buffer and the last buffer in WINDOW."
+    (interactive)
+    (let ((current-buffer (window-buffer window)))
+      ;; If no window is found in the window history, `switch-to-buffer'
+      ;; will fall back to `other-buffer'.
+      (switch-to-buffer
+       (cl-find-if (lambda (buffer)
+                     (not (eq buffer current-buffer)))
+                   (mapcar #'car (window-prev-buffers window)))
+       nil t)))
+
   (defun cfg/toggle-ibuffer ()
     "If the current buffer is *Ibuffer*, use `cfg/alternate-buffer'.
 Otherwise, open `ibuffer'."
@@ -783,10 +798,154 @@ Otherwise, open `ibuffer'."
       (ibuffer)
       ;; (when (minibufferp)
       ;; 	(abort-recursive-edit))
-      (deactivate-mark))))
+      (deactivate-mark)))
+
+  (defun cfg/revert-buffer ()
+    "Revert the current buffer from its file without any confirmation."
+    (interactive)
+    ;; ignore-auto, noconfirm, preserve-modes:
+    (revert-buffer t t t))
+
+  (define-key global-map [remap revert-buffer] #'cfg/revert-buffer)
+
+  ;; Define `cfg/switch-to-buffer' differently depending on whether
+  ;; the `consult' package is available at the time this configuration is loaded.
+  (cond
+   ((fboundp 'consult-buffer)
+    ;; consult is installed and loaded (or will be loaded before this file finishes):
+    (defun cfg/switch-to-buffer ()
+      "Switch to another buffer using `consult-buffer'.
+    If invoked from the minibuffer, abort the minibuffer instead."
+      (interactive)
+      (if (minibufferp)
+          (abort-minibuffers)
+        (call-interactively #'consult-buffer))))
+
+   (t
+    ;; consult is not available -> fall back to the built-in command:
+    (defun cfg/switch-to-buffer ()
+      "Switch to another buffer using the built-in `switch-to-buffer'.
+    If invoked from the minibuffer, abort the minibuffer instead."
+      (interactive)
+      (if (minibufferp)
+          (abort-minibuffers)
+        (call-interactively #'switch-to-buffer)))))
+
+  (define-key global-map [remap switch-to-buffer] #'cfg/switch-to-buffer)
+
+  (defun cfg/kill-other-buffers (&optional arg)
+    "Kill all other buffers.
+If the universal prefix argument ARG is used, then kill the windows too."
+    (interactive "P")
+    (when (yes-or-no-p (format "Killing all buffers except \"%s\"? "
+                               (buffer-name)))
+      (mapc #'kill-buffer (delq (current-buffer) (buffer-list)))
+      (when (equal '(4) arg)
+        (delete-other-windows))
+      (message "Buffers deleted!")))
+
+  (defun cfg/delete-current-buffer-file ()
+    "Remove file connected to current buffer and kill buffer."
+    (interactive)
+    (let ((filename (buffer-file-name))
+          (buffer (current-buffer))
+          (name (buffer-name)))
+      (if (not (and filename (file-exists-p filename)))
+          (ido-kill-buffer)
+        (if (yes-or-no-p
+             (format "Are you sure you want to delete this file: '%s'?" name))
+            (progn
+              (delete-file filename t)
+              (kill-buffer buffer)
+              (message "File deleted: '%s'" filename))
+          (message "Canceled: File deletion")))))
+
+  (defun cfg/rename-current-buffer-file (&optional arg)
+    "Rename the current buffer and the file it is visiting.
+If the buffer isn't visiting a file, ask whether it should be saved
+to a file or just renamed.
+If called without a prefix argument, the prompt is initialized with
+the current directory instead of the filename.  When called with a
+prefix argument ARG, the prompt is initialized with the full
+filename."
+    (interactive "P")
+    (let* ((name (buffer-name))
+           (filename (buffer-file-name)))
+      (if (and filename (file-exists-p filename))
+          ;; the buffer is visiting a file
+          (let* ((dir (file-name-directory filename))
+                 (new-name (read-file-name "New name: " (if arg dir filename))))
+            (cond ((get-buffer new-name)
+                   (error "A buffer named '%s' already exists!" new-name))
+                  (t
+                   (let ((dir (file-name-directory new-name)))
+                     (when (and (not (file-exists-p dir))
+                                (yes-or-no-p
+                                 (format "Create directory '%s'?" dir)))
+                       (make-directory dir t)))
+                   (rename-file filename new-name 1)
+                   (rename-buffer new-name)
+                   (set-visited-file-name new-name)
+                   (set-buffer-modified-p nil)
+                   (when (fboundp 'recentf-add-file)
+                     (recentf-add-file new-name)
+                     (recentf-remove-if-non-kept filename))
+                   (message "File '%s' successfully renamed to '%s'"
+                            name (file-name-nondirectory new-name)))))
+        ;; the buffer is not visiting a file
+        (let ((key))
+          (while (not (memq key '(?s ?r)))
+            (setq key (read-key (propertize
+                                 (format
+                                  (concat "Buffer '%s' is not visiting a file: "
+                                          "[s]ave to file or [r]ename buffer?")
+                                  name)
+                                 'face 'minibuffer-prompt)))
+            (cond ((eq key ?s)            ; save to file
+                   ;; this allows for saving a new empty (unmodified) buffer
+                   (unless (buffer-modified-p) (set-buffer-modified-p t))
+                   (save-buffer))
+                  ((eq key ?r)            ; rename buffer
+                   (let ((new-name (read-string "New buffer name: ")))
+                     (while (get-buffer new-name)
+                       ;; ask to rename again, if the new buffer name exists
+                       (if (yes-or-no-p
+                            (format (concat "A buffer named '%s' already exists: "
+                                            "Rename again?")
+                                    new-name))
+                           (setq new-name (read-string "New buffer name: "))
+                         (keyboard-quit)))
+                     (rename-buffer new-name)
+                     (message "Buffer '%s' successfully renamed to '%s'"
+                              name new-name)))
+                  ;; ?\a = C-g, ?\e = Esc and C-[
+                  ((memq key '(?\a ?\e)) (keyboard-quit))))))))
+
+  (defun cfg/new-empty-buffer ()
+    "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+It returns the buffer (for elisp programing).
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'"
+    (interactive)
+    (let (($buf (generate-new-buffer "untitled")))
+      (switch-to-buffer $buf)
+      (funcall initial-major-mode)
+      (setq buffer-offer-save t)
+      $buf))
+
+  (defun cfg/toggle-maximize-buffer ()
+    "Maximize buffer."
+    (interactive)
+    (save-excursion
+      (if (and (= 1 (length (window-list)))
+               (assoc ?_ register-alist))
+          (jump-to-register ?_)
+        (progn
+          (window-configuration-to-register ?_)
+          (delete-other-windows))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; CONFIGURATION FOR TEMPBUF AND BUFFERS
+;;;; -> TEMPBUF
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Configuration for tempbuf (tempbuf will kill unused buffers after XX seconds) and buffer-menu (M-x Buffer-menu).
@@ -818,169 +977,6 @@ Otherwise, open `ibuffer'."
                 (with-current-buffer "*Async-native-compile-log*"
                   (setq-local tempbuf-minimum-timeout 20)
                   (turn-on-tempbuf-mode))))))
-
-;; defuns:
-
-;; Don't ask for confirmation when refreshing the buffer
-(defun cfg/revert-buffer ()
-  "Revert the current buffer from its file without any confirmation."
-  (interactive)
-  (revert-buffer t t t)) ; ignore-auto, noconfirm, preserve-modes
-(define-key global-map [remap revert-buffer] #'cfg/revert-buffer)
-
-;; Define `cfg/switch-to-buffer' differently depending on whether
-;; the `consult' package is available at the time this configuration is loaded.
-(cond
- ((fboundp 'consult-buffer)
-  ;; consult is installed and loaded (or will be loaded before this file finishes):
-  (defun cfg/switch-to-buffer ()
-    "Switch to another buffer using `consult-buffer'.
-    If invoked from the minibuffer, abort the minibuffer instead."
-    (interactive)
-    (if (minibufferp)
-        (abort-minibuffers)
-      (call-interactively #'consult-buffer))))
-
- (t
-  ;; consult is not available -> fall back to the built-in command:
-  (defun cfg/switch-to-buffer ()
-    "Switch to another buffer using the built-in `switch-to-buffer'.
-    If invoked from the minibuffer, abort the minibuffer instead."
-    (interactive)
-    (if (minibufferp)
-        (abort-minibuffers)
-      (call-interactively #'switch-to-buffer)))))
-
-(define-key global-map [remap switch-to-buffer] #'cfg/switch-to-buffer)
-
-;;;###autoload
-(defun cfg/kill-other-buffers (&optional arg)
-  "Kill all other buffers.
-If the universal prefix argument ARG is used, then kill the windows too."
-  (interactive "P")
-  (when (yes-or-no-p (format "Killing all buffers except \"%s\"? "
-                             (buffer-name)))
-    (mapc #'kill-buffer (delq (current-buffer) (buffer-list)))
-    (when (equal '(4) arg)
-      (delete-other-windows))
-    (message "Buffers deleted!")))
-
-;;;###autoload
-(defun cfg/alternate-buffer (&optional window)
-  "Switch back and forth between the current buffer and the last buffer in WINDOW."
-  (interactive)
-  (let ((current-buffer (window-buffer window)))
-    ;; If no window is found in the window history, `switch-to-buffer'
-    ;; will fall back to `other-buffer'.
-    (switch-to-buffer
-     (cl-find-if (lambda (buffer)
-                   (not (eq buffer current-buffer)))
-                 (mapcar #'car (window-prev-buffers window)))
-     nil t)))
-
-;;;###autoload
-(defun cfg/delete-current-buffer-file ()
-  "Remove file connected to current buffer and kill buffer."
-  (interactive)
-  (let ((filename (buffer-file-name))
-        (buffer (current-buffer))
-        (name (buffer-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (ido-kill-buffer)
-      (if (yes-or-no-p
-           (format "Are you sure you want to delete this file: '%s'?" name))
-          (progn
-            (delete-file filename t)
-            (kill-buffer buffer)
-            (message "File deleted: '%s'" filename))
-        (message "Canceled: File deletion")))))
-
-;;;###autoload
-(defun cfg/rename-current-buffer-file (&optional arg)
-  "Rename the current buffer and the file it is visiting.
-If the buffer isn't visiting a file, ask whether it should be saved
-to a file or just renamed.
-If called without a prefix argument, the prompt is initialized with
-the current directory instead of the filename.  When called with a
-prefix argument ARG, the prompt is initialized with the full
-filename."
-  (interactive "P")
-  (let* ((name (buffer-name))
-         (filename (buffer-file-name)))
-    (if (and filename (file-exists-p filename))
-        ;; the buffer is visiting a file
-        (let* ((dir (file-name-directory filename))
-               (new-name (read-file-name "New name: " (if arg dir filename))))
-          (cond ((get-buffer new-name)
-                 (error "A buffer named '%s' already exists!" new-name))
-                (t
-                 (let ((dir (file-name-directory new-name)))
-                   (when (and (not (file-exists-p dir))
-                              (yes-or-no-p
-                               (format "Create directory '%s'?" dir)))
-                     (make-directory dir t)))
-                 (rename-file filename new-name 1)
-                 (rename-buffer new-name)
-                 (set-visited-file-name new-name)
-                 (set-buffer-modified-p nil)
-                 (when (fboundp 'recentf-add-file)
-                   (recentf-add-file new-name)
-                   (recentf-remove-if-non-kept filename))
-                 (message "File '%s' successfully renamed to '%s'"
-                          name (file-name-nondirectory new-name)))))
-      ;; the buffer is not visiting a file
-      (let ((key))
-        (while (not (memq key '(?s ?r)))
-          (setq key (read-key (propertize
-                               (format
-                                (concat "Buffer '%s' is not visiting a file: "
-                                        "[s]ave to file or [r]ename buffer?")
-                                name)
-                               'face 'minibuffer-prompt)))
-          (cond ((eq key ?s)            ; save to file
-                 ;; this allows for saving a new empty (unmodified) buffer
-                 (unless (buffer-modified-p) (set-buffer-modified-p t))
-                 (save-buffer))
-                ((eq key ?r)            ; rename buffer
-                 (let ((new-name (read-string "New buffer name: ")))
-                   (while (get-buffer new-name)
-                     ;; ask to rename again, if the new buffer name exists
-                     (if (yes-or-no-p
-                          (format (concat "A buffer named '%s' already exists: "
-                                          "Rename again?")
-                                  new-name))
-                         (setq new-name (read-string "New buffer name: "))
-                       (keyboard-quit)))
-                   (rename-buffer new-name)
-                   (message "Buffer '%s' successfully renamed to '%s'"
-                            name new-name)))
-                ;; ?\a = C-g, ?\e = Esc and C-[
-                ((memq key '(?\a ?\e)) (keyboard-quit))))))))
-
-;;;###autoload
-(defun cfg/new-empty-buffer ()
-  "Create a new empty buffer.
-New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
-It returns the buffer (for elisp programing).
-URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'"
-  (interactive)
-  (let (($buf (generate-new-buffer "untitled")))
-    (switch-to-buffer $buf)
-    (funcall initial-major-mode)
-    (setq buffer-offer-save t)
-    $buf))
-
-;;;###autoload
-(defun cfg/toggle-maximize-buffer ()
-  "Maximize buffer."
-  (interactive)
-  (save-excursion
-    (if (and (= 1 (length (window-list)))
-             (assoc ?_ register-alist))
-        (jump-to-register ?_)
-      (progn
-        (window-configuration-to-register ?_)
-        (delete-other-windows)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; -> DIAGNOSTICS
