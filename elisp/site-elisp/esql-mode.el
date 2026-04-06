e1fed452cd0abf646022ba56f35fb939e5161b87
rdbeni0 <rdbeni0.eu@gmail.com>
2026-04-06 21:31:46 +0200

    DATE: 2026-04-06_21:31

--- elisp/site-elisp/esql-mode.el
+++ elisp/site-elisp/esql-mode.el
@@ -20,6 +20,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;;; Code:
+
 (require 'prog-mode)
 
 (defgroup esql nil
