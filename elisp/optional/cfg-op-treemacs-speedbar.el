81c73f146a5d331138d6b6c38fe52b58cbf0b51b
rdbeni0 <rdbeni0.eu@gmail.com>
2026-05-03 20:16:16 +0200

    DATE: 2026-05-03_20:16

--- elisp/optional/cfg-op-treemacs-speedbar.el
+++ elisp/optional/cfg-op-treemacs-speedbar.el
@@ -154,7 +154,8 @@ Do nothing if file is not part of a project."
     (when project
       (funcall orig project)))
 
-  (advice-add 'treemacs-project->position :around #'cfg/-treemacs-safe-project-position))
+  ;; (advice-add 'treemacs-project->position :around #'cfg/-treemacs-safe-project-position)
+  )
 
 (use-package sr-speedbar
   :ensure t
