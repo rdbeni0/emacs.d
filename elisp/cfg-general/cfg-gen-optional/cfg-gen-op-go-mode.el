39e93ff18c05fee42e0138b96897d0757ef83ba2
rdbeni0 <rdbeni0.eu@gmail.com>
2026-08-02 22:04:53 +0200

    DATE: 2026-08-02_22:04

---
+++ elisp/cfg-general/cfg-gen-optional/cfg-gen-op-go-mode.el
@@ -0,0 +1,7 @@
+;;; cfg-gen-op-go-mode.el --- general.el for go/golang -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;;; Code:
+
+(provide 'cfg-gen-op-go-mode)
+;;; cfg-gen-op-go-mode.el ends here
